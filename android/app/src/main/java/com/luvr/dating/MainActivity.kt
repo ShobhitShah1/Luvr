0eb3616bfddd4ed8edef2afc4d260ff0c13e2b58
ShobhitShah1 <shobhit942003@gmail.com>
2024-08-07 21:02:24 +0530

    prepare in app browser on app startup (Android)

--- android/app/src/main/java/com/luvr/dating/MainActivity.kt
+++ android/app/src/main/java/com/luvr/dating/MainActivity.kt
@@ -5,12 +5,18 @@ import com.facebook.react.ReactActivity
 import com.facebook.react.ReactActivityDelegate
 import com.facebook.react.defaults.DefaultNewArchitectureEntryPoint.fabricEnabled
 import com.facebook.react.defaults.DefaultReactActivityDelegate
+import com.proyecto26.inappbrowser.RNInAppBrowserModule
 import org.devio.rn.splashscreen.SplashScreen
 
 class MainActivity : ReactActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         SplashScreen.show(this);
         super.onCreate(null)
+    }
+
+    override fun onStart() {
+        super.onStart()
+        RNInAppBrowserModule.onStart(this)
     }
 
   /**
